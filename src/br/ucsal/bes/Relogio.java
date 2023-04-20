74125961baac6040592c4af95ec4279837275909
noah <117120976+blehzango@users.noreply.github.com>
2023-04-20 15:28:31 -0300

    Update Relogio.java

--- src/br/ucsal/bes/Relogio.java
+++ src/br/ucsal/bes/Relogio.java
@@ -1,5 +1,55 @@
 package br.ucsal.bes;
 
-public class Relogio {
+import java.util.concurrent.atomic.AtomicBoolean;
+public class Relogio implements Runnable{
+    private Integer tempo = 500;
+    private String name="";
+    private static final Integer tempoTotal = 500;
+    private AtomicBoolean done = new AtomicBoolean(false);
+    public AtomicBoolean isStopped=new AtomicBoolean(true);
 
+    @Override
+    public void run() {
+        while (!done.get()) {
+            while (!isStopped.get()) {
+                contar();
+            }
+        }
+    }
+    
+    public void contar() {
+         try {
+             if (tempo>0) {
+                 Partida.print( tempo + " segundos restantes");
+                 Thread.sleep(1000);
+                 tempo--;
+             } else if (tempo==0) {
+                 tempo--;
+                 Partida.print( name + " | acabou o tempo!");
+             } else {
+                 done.set(true);
+             }
+         } catch (InterruptedException e) {
+             e.printStackTrace();
+         }
+    }
+
+    public int getTempo(){
+        if(tempo<=0) {
+            return tempoTotal;
+        }
+        return tempoTotal-tempo;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public boolean isDone() {
+        return done.get();
+    }
 }
